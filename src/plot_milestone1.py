fc8953442b5c77bacf34e40ac21e8a4a5e5ee7bf
Håkon Tansem <hakontan@gmail.com>
2020-02-10 18:20:02 +0100

    Fixed bug in spline.cpp and started writing on report.

--- src/plot_milestone1.py
+++ src/plot_milestone1.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import astropy.units as unit
 import astropy.constants as const
 
+plt.style.use("seaborn-darkgrid")
 
-conversion_factor = 3.08567758e22/1e3 # factor to convert s^-1 to km/s/Mpc
+conversion_factor = 3.08567758e22 / 1e3 # factor to convert s^-1 to km/s/Mpc
 
 def x_to_z(x):
     """
@@ -20,9 +21,6 @@ def x_to_z(x):
 
 
 data = np.loadtxt("cosmology.txt") 
-
-#S_to_GYr = 1.0 / (3.26 *3e8 * 3600 * 24 * 365 * 1e9)
-
 
 
 x = data[:, 0]
@@ -43,23 +41,26 @@ fig, ax = plt.subplots(2, 2)
 ax[0, 0].plot(x, eta.to(unit.Gpc))
 ax[0, 0].set_title(r"$\eta(x)$")
 ax[0, 0].set_xlabel(r"$x=log(a)$")
-ax[0, 0].set_ylabel("Mpc")
+ax[0, 0].set_ylabel("Gpc")
+ax[0, 0].grid()
 
 ax[0, 1].semilogy(x, Hp_of_x)
 ax[0, 1].set_title(r"$\mathcal{H}(x)$")
 ax[0, 1].set_xlabel(r"$x=log(a)$")
 ax[0, 1].set_ylabel("km/s/Mpc")
+ax[0, 1].grid()
 
 ax[1, 0].semilogy(x, H_of_x)
 ax[1, 0].set_title(r"H(x)")
 ax[1, 0].set_xlabel(r"$x=log(a)$")
 ax[1, 0].set_ylabel("km/s/Mpc")
-
+ax[1, 0].grid()
 
 ax[1, 1].loglog(z[np.where(z>0)], H_of_x[np.where(z>0)])
 ax[1, 1].set_title(r"H(z)")
 ax[1, 1].set_xlabel(r"Redshift $z$")
 ax[1, 1].set_ylabel("Km/s/Mpc")
+ax[1, 1].grid()
 plt.tight_layout()
 
 
@@ -69,5 +70,7 @@ ax.plot(x, Omega_Lambda, label=r"$\Omega_\Lambda$(x)")
 ax.plot(x, Omega_R, label=r"$\Omega_r(x)$")
 ax.set_xlabel(r"x=log(a)")
 ax.legend()
+ax.grid()
+
 
 plt.show()
