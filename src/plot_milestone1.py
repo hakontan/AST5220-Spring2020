39ab0c26ec802a32bf1ed1b09b40c62e93f44de6
Håkon Tansem <hakontan@gmail.com>
2020-02-19 16:38:25 +0100

    Roughly finished the report for milestone 1. Maybe some minor corrections needed

--- src/plot_milestone1.py
+++ src/plot_milestone1.py
@@ -5,6 +5,17 @@ import astropy.constants as const
 
 plt.style.use("seaborn-darkgrid")
 
+fonts = {
+    "font.family": "serif",
+    "axes.labelsize": 10,
+    "font.size": 10,
+    "legend.fontsize": 10,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
+}
+
+plt.rcParams.update(fonts)
+
 conversion_factor = 3.08567758e22 / 1e3 # factor to convert s^-1 to km/s/Mpc
 
 def x_to_z(x):
@@ -26,6 +37,7 @@ data = np.loadtxt("cosmology.txt")
 x = data[:, 0]
 eta = data[:, 1] * unit.m
 eta = eta.decompose()
+eta = eta.to(unit.Gpc)
 Hp_of_x = data[:, 2] * conversion_factor
 H_of_x = data[:, 3] * conversion_factor
 Omega_B = data[:, 5]
@@ -38,7 +50,7 @@ z = x_to_z(x)
 
 fig, ax = plt.subplots(2, 2)
 
-ax[0, 0].plot(x, eta.to(unit.Gpc))
+ax[0, 0].semilogy(x, eta.value)
 ax[0, 0].set_title(r"$\eta(x)$")
 ax[0, 0].set_xlabel(r"$x=log(a)$")
 ax[0, 0].set_ylabel("Gpc")
@@ -62,6 +74,7 @@ ax[1, 1].set_xlabel(r"Redshift $z$")
 ax[1, 1].set_ylabel("Km/s/Mpc")
 ax[1, 1].grid()
 plt.tight_layout()
+fig.savefig("../doc/milestoneI/figures/evolution.pdf", dpi=1000)
 
 
 fig, ax = plt.subplots()
@@ -69,8 +82,10 @@ ax.plot(x, Omega_B + Omega_CDM, label=r"$\Omega_b(x) + \Omega_{CDM}(x)$")
 ax.plot(x, Omega_Lambda, label=r"$\Omega_\Lambda$(x)")
 ax.plot(x, Omega_R, label=r"$\Omega_r(x)$")
 ax.set_xlabel(r"x=log(a)")
+ax.set_ylabel("Fraction of total energy content")
 ax.legend()
 ax.grid()
+fig.savefig("../doc/milestoneI/figures/densityparams.pdf", dpi=1000)
 
 
 plt.show()
